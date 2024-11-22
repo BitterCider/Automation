9cc3813064e8411145d1f203aaccef3734b379d5
Tal Baruchi <talbaruchy@gmail.com>
2024-11-22 15:17:46 +0200

    לבדיקה

--- tests/sanityTests/sanityTests.spec.js
+++ tests/sanityTests/sanityTests.spec.js
@@ -17,42 +17,48 @@ import {
 } from "../Variables";
 
 test.describe("sanityTest", () => {
-  test.beforeEach(async ({ page }) => {
+    
+  test("endToEnd", async ({ page }) => {
+
+  //Navigate to site and log in using valid credentials
     await page.goto(swagLabsURL);
     await page.locator("#user-name").fill("standard_user");
     await page.locator("#password").fill("secret_sauce");
     await page.locator("#login-button").click();
-  });
 
-  test("login&verify", async ({ page }) => {
+  //Assert site URL after log in
     await expect(page).toHaveURL(inventoryURL);
     await expect(page.locator(".app_logo")).toHaveText("Swag Labs");
-  });
 
-  test("addToCart", async ({ page }) => {
+  //Add backpack and jacket to shopping cart and assert their names
     await page.locator(bagToCart).click();
     await expect(page.locator(bagLink)).toContainText("Sauce Labs Backpack");
     await page.locator(jacketToCart).click();
-    await expect(page.locator(jacketLink)).toContainText(
-      "Sauce Labs Fleece Jacket"
-    );
-    await expect(page.locator(cartBadge)).toContainText("2");
+    await expect(page.locator(jacketLink)).toContainText("Sauce Labs Fleece Jacket");
 
+  //Assert cart badge represents number of items added
+    await expect(page.locator(cartBadge)).toContainText("2");
+  
+  //Navigate to shopping cart page and assert 2 items in list
     await page.locator(cartIconLink).click();
     await expect(page.locator(itemQuantity)).toHaveCount(2);
-    await page.locator("#checkout").click();
 
+  //Proceed to checkout and assert site URL and title
+    await page.locator("#checkout").click();
     await expect(page).toHaveURL(stepOneURL);
-    await expect(page.locator(".title")).toHaveText(
-      "Checkout: Your Information"
-    );
+    await expect(page.locator(".title")).toHaveText("Checkout: Your Information");
+
+  //Fill required details
     await page.locator("#first-name").fill("tal");
     await page.locator("#last-name").fill("baruchi");
     await page.locator("#postal-code").fill("2220325");
-    await page.locator("#continue").click();
 
+  //Proceed to overview page and assert site URL and title
+    await page.locator("#continue").click();
     await expect(page).toHaveURL(stepTwoURL);
     await expect(page.locator(".title")).toHaveText("Checkout: Overview");
+  
+  //Proceed to final page, assert URL, title, header and text
     await page.locator("#finish").click();
     await expect(page).toHaveURL(finishURL);
     await expect(page.locator(".title")).toHaveText("Checkout: Complete!");

95df26f596d51f8ee12434131daa55fec5b965df
Tal Baruchi <talbaruchy@gmail.com>
2024-11-24 15:09:07 +0200

    לבדיקה

--- tests/negativeTests/negativeTests.spec.js
+++ tests/negativeTests/negativeTests.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 import { swagLabsURL, errorElement, userObjects } from "../Variables";
 
-test.describe.only("negativeTests", () => {
+test.describe("negativeTests", () => {
   for (let user of userObjects) {
     test(`Invalid Userdata logins: ${
       user.userName === "" ? "Empty username" : user.userName
